feb54677fb069e14e4150480da6d181d02bcfa6c
andyehrenberg <32784181+andyehrenberg@users.noreply.github.com>
2018-11-21 13:43:41 -0800

    Update eye_track.py

--- eye_track.py
+++ eye_track.py
@@ -4,28 +4,6 @@ Created on Sat Nov 10 17:03:21 2018
 
 @author: andye
 """
-#!/usr/bin/python
-
-# Copyright (c) 2015 Matthew Earl
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-#     The above copyright notice and this permission notice shall be included
-#     in all copies or substantial portions of the Software.
-# 
-#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-#     NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-#     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-#     OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-#     USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 import dlib
 import cv2
 import numpy
@@ -35,23 +13,13 @@ path = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(path)
 
-#class TooManyFaces(Exception):
-#    pass
-#
-#class NoFaces(Exception):
-#    pass
-
 def get_landmarks(im):
     #first detect face
     rects = detector(im, 1)
     
     #cases for if multiple or zero faces are detected
-    if len(rects) > 1:
+    if len(rects) != 1:
         return numpy.matrix([0])
-        #raise TooManyFaces
-    if len(rects) == 0:
-        return numpy.matrix([0])
-        #raise NoFaces
         
     #return matrix of coordinates of facial landmarks
     return numpy.matrix([[pt.x, pt.y] for pt in predictor(im, rects[0]).parts()])
