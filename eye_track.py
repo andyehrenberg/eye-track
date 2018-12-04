04a3960d8dc087ab13149d4380f5f04fc1b3a295
andyehrenberg <32784181+andyehrenberg@users.noreply.github.com>
2018-12-04 15:09:09 -0800

    better method of increasing FPS

--- eye_track.py
+++ eye_track.py
@@ -15,52 +15,36 @@ path = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(path)
 
-def get_landmarks(im, roi):
+def get_landmarks(im):
     
     #first detect face
-    if roi != None:
-        im = im[roi[2]:roi[3],roi[0]:roi[1]]
-    else:
-        pass
-        #img = im.copy()
+    small = im.copy()
+    small = cv2.resize(small, (0, 0), fx=0.25, fy=0.25)
     start = time.time()
-    rects = detector(im, 1)
+    rects = detector(small, 1)
     end = time.time()
     #print('time to get face:',end-start)
     
-    
     if len(rects) == 0:
         roi = None
-        return np.matrix([0]), roi
+        return np.matrix([0])
     
     i = 0
-    area = rects[0].area
+    area = rects[0].area()
     for j in range(1,len(rects)):
-        temp = rects[j].area
+        temp = rects[j].area()
         if temp > area:
             i = j
             area = temp
             
-    left = rects[i].left()
-    right = rects[i].right()
-    top = rects[i].top()
-    bottom = rects[i].bottom()
-    dx = right - left
-    dy = bottom - top
-    left = max(0, int(left - dx*0.2))
-    right = int(right + dx*0.2)
-    top = max(0, int(top - dy*0.2))
-    bottom = int(bottom + dy*0.2)
+    left = rects[i].left()*4
+    right = rects[i].right()*4
+    top = rects[i].top()*4
+    bottom = rects[i].bottom()*4
     
-    roi = [left, right, top, bottom]
+    rect = dlib.rectangle(left, top, right, bottom)
 
-    #return matrix of coordinates of facial landmarks
-    start = time.time()
-    x = np.matrix([[pt.x, pt.y] for pt in predictor(im, rects[i]).parts()])
-    end = time.time()
-    #print('time to get landmarks: ',end - start)
-    
-    return x, roi
+    return np.matrix([[pt.x, pt.y] for pt in predictor(im, rect).parts()])
 
 def get_eyes(im, landmarks):
     img = im.copy()
@@ -76,8 +60,8 @@ def get_eyes(im, landmarks):
     
     #only get eyes if they are found   
     if len(landmarks) > 1:
+        print('getting eyes')
         start = time.time()
-        #print(landmarks)
         #get the landmarks corresponding to the left and right eye
         right_points = landmarks[36:42]
         left_points = landmarks[42:48]
@@ -88,8 +72,8 @@ def get_eyes(im, landmarks):
         lv_max = max([left_points[4,1], left_points[5,1]])
         rh_delta = int((right_points[3,0] - right_points[0,0])*0.3)
         lh_delta = int((left_points[3,0] - left_points[0,0])*0.3)
-        rv_delta = int((rv_max - rv_min)*0.3)
-        lv_delta = int((lv_max - lv_min)*0.3)
+        rv_delta = int((rv_max - rv_min)*0.4)
+        lv_delta = int((lv_max - lv_min)*0.4)
         
         #get a rectangular region for both eyes, covert to greyscale
         right_eye = im[rv_min-rv_delta:rv_max+rv_delta,right_points[0,0]-rh_delta:right_points[3,0]+rh_delta]
@@ -108,12 +92,10 @@ def get_eyes(im, landmarks):
         cv2.imshow('right',right_eye)
         end = time.time()
         #print('time to get eye images: ',end - start)
-        return im, left_eye, right_eye
+        return img, left_eye, right_eye
     
     else:
-        return [im]
-
-roi = None
+        return [img]
 
 vid_get = VideoGet(0).start()
 
@@ -122,24 +104,18 @@ while 1:
     #ret, img = cap.read() #read image from webcam
     img = vid_get.frame
     #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #get greyscale for face detection
-    landmarks, roi_ = get_landmarks(img, roi)
-    if roi_ == None or roi == None:    
-        with_landmarks = get_eyes(img, landmarks)
-    else:
-        with_landmarks = get_eyes(img[roi[2]:roi[3],roi[0]:roi[1]], landmarks)
-    s1 = time.time()
+    landmarks = get_landmarks(img)
+    with_landmarks = get_eyes(img, landmarks)
     try:
         cv2.imshow('img', with_landmarks[0])
     except:
         pass
-    e1 = time.time()
     #print('time to show image:',e1 - s1)
     k = cv2.waitKey(30) & 0xff
     if k == 27 or vid_get.stopped:
         vid_get.stop()
         break
     end = time.time()
-    roi = roi_
     print(1./(end - start))
 
 cv2.destroyAllWindows()
